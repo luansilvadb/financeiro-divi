9c82b2970fa10b0f3315526ab57579aec73ad5cf
luansilvadb <contato@luansilva.com.br>
2026-05-17 18:15:03 -0300

    style: configure FAB transition to pure zoom in/out without fade

--- src/App.vue
+++ src/App.vue
@@ -166,9 +166,12 @@ const handleSalvarTransacao = async () => {
 
     <!-- Floating Action Button (FAB) -->
     <transition
-      enter-active-class="transition duration-500 delay-300 ease-out"
-      enter-from-class="opacity-0 scale-50"
-      enter-to-class="opacity-100 scale-100"
+      enter-active-class="transition duration-300 ease-out"
+      enter-from-class="scale-0"
+      enter-to-class="scale-100"
+      leave-active-class="transition duration-200 ease-in"
+      leave-from-class="scale-100"
+      leave-to-class="scale-0"
     >
       <Button
         v-if="currentView === 'dashboard'"
